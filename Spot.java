b10c95e401a1e6d3851e90ef86e4c956774bd53d
WillDevGit <williansilvaws063@gmail.com>
2023-06-12 21:34:36 -0300

    Classe Completa

--- Spot.java
+++ Spot.java
@@ -2,6 +2,10 @@
  * Classe contendo ações e informações sobre cada espaço (quadrado) no tabuleiro
  */
 public class Spot {
+    private Piece piece; 
+    private Position pos; 
+    private Color color;
+    
     /**
      * Construtor para espaços com peça e com cor
      * @param piece Peça que inicia nesse espaço do tabuleiro
@@ -9,7 +13,9 @@ public class Spot {
      * @param color Cor do espaço no tabuleiro (Templo)
      */
     public Spot(Piece piece, Position pos, Color color) {
-    
+        this.piece = piece; 
+        this.pos = pos; 
+        this.color = color;
     }
 
     /**
@@ -18,7 +24,9 @@ public class Spot {
      * @param pos Posição do espaço no tabuleiro
      */
     public Spot(Piece piece, Position pos) {
-
+        this.piece = piece; 
+        this.pos = pos; 
+        this.color = color.NONE; 
     }
 
     /**
@@ -26,7 +34,9 @@ public class Spot {
      * @param pos Posição do espaço no tabuleiro
      */
     public Spot(Position pos) {
-
+        this.color = color.NONE;
+        this.piece = null; 
+        this.pos = pos;  
     }
 
     /**
@@ -34,7 +44,7 @@ public class Spot {
      * @return Objeto Position contendo a posição (coordenadas) do espaço
      */
     public Position getPosition() {
-        return null;
+        return this.pos;
     }
 
     /**
@@ -42,7 +52,7 @@ public class Spot {
      * @return Objeto Piece caso tenha uma peça ou null caso o espaço esteja vazio
      */
     public Piece getPiece() {
-        return null;
+        return this.piece;
     }
 
     /**
@@ -50,7 +60,7 @@ public class Spot {
      * @return Enum Color com a cor do espaço. Caso o espaço não tenha cor, o valor do enum será NONE
      */
     public Color getColor() {
-        return null;
+        return this.color;
     }
 
     /**
@@ -59,13 +69,17 @@ public class Spot {
      * @exception IllegalMovementException Caso o espaço já esteja ocupado por uma peça da mesma cor
      */
     protected void occupySpot(Piece piece) throws IllegalMovementException {
-
+        if(this.piece.getColor() == piece.getColor()){
+            throw new IllegalMovementException("Espaço ocupado por uma peça da mesma cor");
+        }
+        this.piece = piece; 
     }
 
     /**
      * Método que "libera" o espaço atual, ou seja, deixa-o vazio
      */
     protected void releaseSpot() {
-
+        this.color = color.NONE; 
+        this.piece = null;
     }
 }
